d53d67e38c363c476b80e47250eb8b07c61bc589
cheddarr <83439317+hicheddar@users.noreply.github.com>
2022-08-08 15:56:38 -0700

    Add files via upload

---
+++ dds2pngconv.py
@@ -0,0 +1,13 @@
+# chedsapp
+# ched#1234 on discord
+import os
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+directory = askdirectory(title='select directory with .dds files')
+print(directory)
+
+for v in os.listdir(directory):
+	f = os.path.join(directory, v)
+	if os.path.isfile(f) and f[-4:]==".dds":
+		pre, ext = os.path.splitext(f)
+		os.rename(f, pre + ".png")
